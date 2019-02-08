529725fbedbd8ca5c1d546215b1de5871c5cd96c
Evyatar <evyatar.a@fiverr.com>
2019-02-09 01:45:23 +0200

    Updates

---
+++ precache-manifest.7ad821cce06b6e13f0f45523e9cf2d00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7f1505f63c8fd21b76d1874456564d7",
+    "url": "/static/media/my-face.d7f1505f.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ebb6788aede38e094c62",
+    "url": "/static/js/main.ebb6788a.chunk.js"
+  },
+  {
+    "revision": "d0e9cc97cc7619ffa63c",
+    "url": "/static/js/1.d0e9cc97.chunk.js"
+  },
+  {
+    "revision": "ebb6788aede38e094c62",
+    "url": "/static/css/main.9d72507e.chunk.css"
+  },
+  {
+    "revision": "5fdcbe39516d74ba199d20107dae76e6",
+    "url": "/index.html"
+  }
+];

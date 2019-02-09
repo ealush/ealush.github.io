902d9018254d4cb1a3bdf91e1e8fea4e855cc0f6
Evyatar <evyatar.a@fiverr.com>
2019-02-09 11:59:11 +0200

    Updates

---
+++ precache-manifest.94158c1daa0e6c2c1e07dc8a0eb851d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7f1505f63c8fd21b76d1874456564d7",
+    "url": "/static/media/my-face.d7f1505f.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9a621273b80a0bd7fc40",
+    "url": "/static/js/main.9a621273.chunk.js"
+  },
+  {
+    "revision": "4dd138c942b218968a4d",
+    "url": "/static/js/1.4dd138c9.chunk.js"
+  },
+  {
+    "revision": "c9b3801c1f9358f5f5db7884975ebf9d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ad821cce06b6e13f0f45523e9cf2d00.js"
+  "/precache-manifest.94158c1daa0e6c2c1e07dc8a0eb851d7.js"
 );
 
 workbox.clientsClaim();

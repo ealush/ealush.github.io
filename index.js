a89aaeb9b5d48970c384e88e920c2fd705725152
ealush <code@ealush.com>
2019-07-22 23:40:41 +0300

    Use vent

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const convert = (oldMin, oldMax, newMin, newMax, oldValue) => (
     (((oldValue - oldMin) * (newMax - newMin)) / (oldMax - oldMin)) + newMin
 );
 
-window.addEventListener('scroll', (e) => {
+vent(window).on('scroll', (e) => {
     const mainHeight = $main.clientHeight;
     const posY = convert(0, mainHeight, 0, 100, window.scrollY);
     $main.style.backgroundPositionY = `${posY}%`;
 });
 
-document.querySelector('h2').addEventListener('click', ({ target }) => {
+vent('h2').on('click', ({ target }) => {
     target.scrollIntoView({
         behavior: 'smooth',
         block: 'start'

65c7a4bcae83a97566bb88cf483123049402d873
Evyatar <code@ealush.com>
2022-12-07 13:38:01 +0200

    Update push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,5 +2,5 @@ git config --global user.email $EMAIL_ADDRESS --replace-all
 git config --global user.name ealush
 
 git add .
-git commit -m "$(curl -s whatthecommit.com/index.txt)"
+git commit -m "$(curl -s https://whatthecommit.com/index.txt)"
 git push https://$GITHUB_TOKEN@github.com/ealush/ealush.github.io.git master
